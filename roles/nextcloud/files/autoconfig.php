5e5f7d2246ee6454bdb05dc268e9ec30b9a7e329
Aditya Patawari <aditya@adityapatawari.com>
2017-06-04 16:21:59 +0530

    setting up nextcloud

---
+++ roles/nextcloud/files/autoconfig.php
@@ -0,0 +1,12 @@
+<?php
+$AUTOCONFIG = array(
+  "dbtype"        => "mysql",
+  "dbname"        => "nextcloud",
+  "dbuser"        => "oc_nextadmin",
+  "dbpass"        => "nextpa55wd",
+  "dbhost"        => "139.59.23.144",
+  "dbtableprefix" => "nc_",
+  "adminlogin"    => "nextadmin",
+  "adminpass"     => "nextcloud",
+  "directory"     => "/var/www/html/nextcloud/data",
+);
